ae7e69e0239c6c781b724509c03e62f35010da1f
alco99 <76519958+alco99@users.noreply.github.com>
2022-05-25 22:50:18 +0800

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,30 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>這裡放標題</title>
+		<?php # 一些 PHP 程式碼
+		require_once 'db_connect.php';
+		require_once 'function.php';
+		
+		
+		
+		?>
+	</head>
+	<body>
+		<?php # 一些 PHP 程式碼
+		session_destroy();
+		
+
+		
+		
+		
+
+		?>
+	</body>
+</html> 
+<?php # 一些 PHP 函式放這邊
+
+
+?>
