dd69caf90712518e4b34f21a6874068407f62e7a
alco99 <76519958+alco99@users.noreply.github.com>
2022-05-28 00:01:09 +0800

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,6 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<meta http-equiv="refresh" content="3;url=index.php"/> <!--跳轉-->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
 		<title>這裡放標題</title>
@@ -12,12 +11,25 @@
 		require_once 'function.php';
 		
 		
+		if (isset($_POST["登入"]) && ($_POST["登入"]!="")) {
+			//檢查帳號密碼
+			//無帳號則顯示錯誤
+			//密碼錯誤
+			//登入成功
+			$_SESSION['login'] = '1';
+			
+			//echo $_SESSION['login'];
+		}
 		
+		if (isset($_POST["註冊"]) && ($_POST["send"]=="註冊")) {
+			
+		}
 		?>
 	</head>
 	<body>
 		<div class="container">	
 			<?php # 一些 PHP 程式碼
+				
 				echo '<div class="row">';
 					//logo
 					echo '<a href = "index.php" >';
@@ -26,13 +38,17 @@
 				echo '</div>';
 				
 				echo '<div class="row">';
+					echo '<hr/><br/>會員登入頁面<br/><br/><br/>';
+				echo '</div>';
+				
+				echo '<div class="row">';
 					echo '<form action = "" method = "post">';
-						echo 'Account ：';
-						echo '<input type = "text" name = "account">';
-						echo '<br/>';
-						echo 'password：';
-						echo '<input type = "password" name = "password">';
-						echo '<br/>';
+						echo 'Account ：<input type = "text" name = "account">';
+						echo '<br/><br/>';
+						echo 'password：<input type = "password" name = "password">';
+						echo '<br/><br/>';
+						echo '<input type = "submit" value = "登入" name = "accountLogin">  ';
+						echo '  <input type = "submit" value = "註冊" name = "signup">';
 					echo '</form>';
 					
 					$_SESSION['login'] = '1';
