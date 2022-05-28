1e175b5d6a6415131ba5d94ff1531f7d6e7702b1
Lai-14 <76561694+Lai-14@users.noreply.github.com>
2022-05-28 23:24:38 +0800

    Update

--- member.php
+++ member.php
@@ -10,6 +10,40 @@
 		require_once 'db_connect.php';
 		require_once 'function.php';
 		
+		// 判斷顯示的頁面
+		$choiceResult = 0;
+		if(isset($_POST['choice']) && ($_POST["choice"]=="newpassword")){	// 修改密碼
+			$choiceResult = 1;
+		} else if(isset($_POST['choice']) && ($_POST["choice"]=="newdata")){	// 修改會員資料
+			$choiceResult = 2;		
+		} else if(isset($_POST['choice']) && ($_POST["choice"]=="order")){	// 訂單查詢
+			$choiceResult = 3;
+		}
+		
+		// 修改密碼或會員資料
+//		echo $_SESSION['UserID'];	// test
+		if(isset($_POST["save"]) && ($_POST["save"]=="儲存")){
+			if(isset($_POST["choiceResult"]) && $_POST["choiceResult"] == 1){	// 修改密碼
+				if(isset($_POST['oldPsw'])){
+					$inPassword = searchPassword($_SESSION['UserID'], $_POST['oldPsw']);	// 尋找密碼是否存在
+					if($inPassword == ""){		// 原本密碼輸入錯誤
+						echo 'Old password 輸入錯誤';
+					}else{
+						if(isset($_POST["newPsw"]) && isset($_POST["newPswA"]) && ($_POST["newPsw"] == $_POST["newPswA"])){		// newPsw == newPswA
+							updatePassword($_SESSION['UserID'], $_POST["newPsw"]);
+							echo '修改密碼成功';
+						}else{
+							echo 'New password again 輸入錯誤';
+						}
+					}
+				}
+			}else if (isset($_POST["choiceResult"]) && $_POST["choiceResult"] == 2){	// 修改會員資料
+				if(isset($_POST['newName']) && isset($_POST['newEmail'])){
+					updateMemberData($_SESSION['UserID'], $_POST['newName'], $_POST['newEmail']);
+					echo '修改資料成功';
+				}
+			}
+		}
 		
 		?>
 	</head>
@@ -66,38 +100,35 @@
 					echo '</div>';
 					// 選擇功能後顯示頁面
 					echo '<div class="col-9">';
-						if($_POST["choice"]=="newpassword") {	// 修改密碼
+						if($choiceResult == 1) {	// 修改密碼
 							echo'<div class="row">';
 								echo '<br/>修改密碼<br/><br/><br/>';
 							echo'</div>';
 							
-							echo'<div>';
+							echo '<form action = "" method = "post">';
 								echo 'Old password： <input type = "password" name = "oldPsw">';
 								echo '<br/><br/>';
-								
 								echo 'New password： <input type = "password" name = "newPsw">';
 								echo '<br/><br/>';
-								
 								echo 'New password again： <input type = "password" name = "newPswA">';
 								echo '<br/><br/>';
-								
-								echo '<input type = "submit" value = "Send">'; 
-							echo'</div>';
-						}else if($_POST["choice"]=="newdata") {		// 修改會員資料
+								echo '<input type = "hidden" name = "choiceResult" value = "'.$choiceResult.'">';
+								echo '<input type = "submit" value = "儲存" name="save">'; 
+							echo '</form>';
+						}else if($choiceResult == 2) {		// 修改會員資料
 							echo'<div class="row">';
 								echo '<br/>修改會員資料<br/><br/><br/>';
 							echo'</div>';
 							
-							echo'<div>';
-								echo 'Name： <input type = "password" name = "newName">';
+							echo '<form action = "" method = "post">';
+								echo 'Name： <input type = "text" name = "newName">';
 								echo '<br/><br/>';
-								
-								echo 'Email： <input type = "password" name = "newEmail">';
+								echo 'Email： <input type = "text" name = "newEmail">';
 								echo '<br/><br/>';
-								
-								echo '<input type = "submit" value = "Send">';
-							echo'</div>';
-						}else if($_POST["choice"]=="order") {		// 訂單查詢
+								echo '<input type = "hidden" name = "choiceResult" value = "'.$choiceResult.'">';
+								echo '<input type = "submit" value = "儲存" name="save">';
+							echo '</form>';
+						}else if($choiceResult == 3) {		// 訂單查詢
 							echo'<div class="row">';
 								echo '<br/>訂單查詢<br/><br/><br/>';
 							echo'</div>';
