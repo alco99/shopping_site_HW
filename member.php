e3bb5a38e0699f574b850719a72a9a17c3b5d798
Lai-14 <76561694+Lai-14@users.noreply.github.com>
2022-05-28 17:17:58 +0800

    Update member

--- member.php
+++ member.php
@@ -5,11 +5,10 @@
 		<meta charset="UTF-8">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
-		<title>這裡放標題</title>
+		<title>會員資料</title>
 		<?php # 一些 PHP 程式碼
 		require_once 'db_connect.php';
 		require_once 'function.php';
-		
 		
 		
 		?>
@@ -36,7 +35,7 @@
 					//登出
 					echo '<div class="col-2">';
 						echo '<a href = "login.php" >';
-						echo '<img src = "images/login.png">';
+						echo '<img src = "images/logout.png">';
 						echo '</a>';
 						
 					echo '</div>';
@@ -65,8 +64,75 @@
 							
 						echo '</form>';
 					echo '</div>';
+					// 選擇功能後顯示頁面
 					echo '<div class="col-9">';
-						
+						if($_POST["choice"]=="newpassword") {	// 修改密碼
+							echo'<div class="row">';
+								echo '<br/>修改密碼<br/><br/><br/>';
+							echo'</div>';
+							
+							echo'<div>';
+								echo 'Old password： <input type = "password" name = "oldPsw">';
+								echo '<br/><br/>';
+								
+								echo 'New password： <input type = "password" name = "newPsw">';
+								echo '<br/><br/>';
+								
+								echo 'New password again： <input type = "password" name = "newPswA">';
+								echo '<br/><br/>';
+								
+								echo '<input type = "submit" value = "Send">'; 
+							echo'</div>';
+						}else if($_POST["choice"]=="newdata") {		// 修改會員資料
+							echo'<div class="row">';
+								echo '<br/>修改會員資料<br/><br/><br/>';
+							echo'</div>';
+							
+							echo'<div>';
+								echo 'Name： <input type = "password" name = "newName">';
+								echo '<br/><br/>';
+								
+								echo 'Email： <input type = "password" name = "newEmail">';
+								echo '<br/><br/>';
+								
+								echo '<input type = "submit" value = "Send">';
+							echo'</div>';
+						}else if($_POST["choice"]=="order") {		// 訂單查詢
+							echo'<div class="row">';
+								echo '<br/>訂單查詢<br/><br/><br/>';
+							echo'</div>';
+							
+							echo'<div class="row">';
+								echo'<div class="col-2">';
+									echo '查詢條件 : ';
+								echo'</div>';								
+								// 未出貨
+								echo'<div class="col-2">';
+									echo '<input type = "radio" name = "choice" value = "noShipping" onchange="this.form.submit()">';
+									echo '未出貨';
+								echo'</div>';
+								
+								// 已出貨
+								echo'<div class="col-2">';
+									echo '<input type = "radio" name = "choice" value = "shipping" onchange="this.form.submit()">';
+									echo '已出貨';
+								echo'</div>';
+								
+								// 已完成
+								echo'<div class="col-2">';
+									echo '<input type = "radio" name = "choice" value = "finish" onchange="this.form.submit()">';
+									echo '已完成';
+								echo'</div>';
+								
+								// 已評價
+								echo'<div class="col-2">';
+									echo '<input type = "radio" name = "choice" value = "evaluate" onchange="this.form.submit()">';
+									echo '已評價';
+								echo'</div>';
+								
+								echo '<br/><hr/>';
+							echo'</div>';
+						}
 					echo '</div>';
 				echo '</div>';
 					
