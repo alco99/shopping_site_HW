ac34df811413f1532a097f0ed664e22b84e779f7
alco99 <76519958+alco99@users.noreply.github.com>
2022-05-28 12:28:53 +0800

    Update login.php

--- login.php
+++ login.php
@@ -5,24 +5,42 @@
 		<meta charset="UTF-8">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
-		<title>這裡放標題</title>
+		<title>登入頁面</title>
 		<?php # 一些 PHP 程式碼
 		require_once 'db_connect.php';
 		require_once 'function.php';
 		
 		
-		if (isset($_POST["登入"]) && ($_POST["登入"]!="")) {
+		if (isset($_POST["accountLogin"]) && ($_POST["accountLogin"]=="登入")) {
 			//檢查帳號密碼
-			//無帳號則顯示錯誤
-			//密碼錯誤
-			//登入成功
-			$_SESSION['login'] = '1';
-			
-			//echo $_SESSION['login'];
+			if(isset($_POST["account"]) && isset($_POST["password"])){
+				$memdata = getMember($_POST["account"]);
+				if($memdata == ""){//無帳號則顯示錯誤
+					echo "帳號輸入錯誤";
+					
+				} else {
+					if($_POST["password"] == $memdata['Password'][0]){//登入成功
+						$_SESSION['login'] = '1';
+						$_SESSION['UserID'] = $memdata['ID'][0];
+						echo '登入成功<br/>';
+					} else {//密碼錯誤
+						echo "密碼輸入錯誤";
+					}
+				}
+			}
 		}
 		
-		if (isset($_POST["註冊"]) && ($_POST["send"]=="註冊")) {
-			
+		if (isset($_POST["signup"]) && ($_POST["signup"]=="註冊")) {
+			if(isset($_POST["account"]) && isset($_POST["password"])){
+				$memdata = getMember($_POST["account"]);
+				if($memdata == ""){//無帳號則顯示錯誤
+					$_SESSION['UserID'] = signUpSet($_POST["account"], $_POST["password"]);
+					echo '註冊成功<br/>';
+					$_SESSION['login'] = '1';
+				} else {
+					echo "帳號重複或已有會員";
+				}
+			}
 		}
 		?>
 	</head>
@@ -50,11 +68,7 @@
 						echo '<input type = "submit" value = "登入" name = "accountLogin">  ';
 						echo '  <input type = "submit" value = "註冊" name = "signup">';
 					echo '</form>';
-					
-					$_SESSION['login'] = '1';
-					//echo $_SESSION['login'];
 				echo '</div>';
-			
 			?>
 		</div>	
 	</body>
