428ee933adb6ecef96fe5fce6936973d2b15e4b2
alco99 <76519958+alco99@users.noreply.github.com>
2022-05-29 17:27:53 +0800

    Update function.php

--- function.php
+++ function.php
@@ -71,6 +71,7 @@ function getAllProduct($sort = 'ID', $pat = 'DESC') {
 	return $rtn;
 }
 
+//從ID搜尋完整商品資料
 function getProductformID($id) {
 	global $dblink;
 	$exam_sql  = "
@@ -93,7 +94,7 @@ function getProductformID($id) {
 	return $rtn;
 }
 
-//立刻跳出加入購物車資訊
+//立刻跳出資訊
 function ajaxFormJs($idName,$span) { # Ajax 表單 , 需要 jquery
 	if ($span) $rtn='<span id="Rlt'.$idName.'"></span>'; else $rtn=''; 
 	$rtn.="<script type=\"text/javascript\">  
@@ -147,6 +148,23 @@ function selectCart($act, $pro) {
 		$idx++;
 	}
 	return $rtn;
+}
+
+function getThisCart($act, $id) {
+	global $dblink;
+	$exam_sql  = "
+				SELECT * 
+				FROM cart 
+				WHERE UserID = '$act' and ID = '$id'
+				ORDER BY ID DESC
+				";
+	$chk_rlt=mysqli_query($dblink,$exam_sql); if (FALSE===$chk_rlt) echo __LINE__ . mysqli_error($dblink);
+	$idx=0;
+	$rtn = NULL;
+	$row = mysqli_fetch_assoc($chk_rlt);
+	
+	
+	return $row;
 }
 
 //會員資訊
@@ -203,6 +221,47 @@ function newProCart($usr, $pro, $name, $price, $proQ) {
 			";//UserID	ProductID ProductName ProductPrice ProductQuantity
 	$inst_result=mysqli_query($dblink,$inst_sql); if (FALSE===$inst_result) echo __LINE__ . mysqli_error($dblink);
 	return  mysqli_insert_id($dblink);
+}
+
+//新訂單
+function newOrder($ordN, $pro, $usr, $proQ, $add, $name, $pho, $pay, $sent, $bill) {
+	global $dblink;
+	$inst_sql = "
+			INSERT INTO  `order`
+			( OrderNum, ProductID, UserID, Amount, ReceiveAddress, ReceiveName, ReceivePhone, Payment, Sent, Bill) 
+			VALUES ( '$ordN', '$pro', '$usr', '$proQ', '$add', '$name', '$pho', '$pay', '$sent', '$bill')
+			";//OrderNum	ProductID	UserID	Amount	ReceiveAddress	ReceiveName	ReceivePhone	Payment	Sent
+	$inst_result=mysqli_query($dblink,$inst_sql); if (FALSE===$inst_result) echo __LINE__ . mysqli_error($dblink);
+	return  mysqli_insert_id($dblink);
+}
+
+//搜尋用戶過往訂單的訂單編號
+function getOldOrder($id) {
+	global $dblink;
+	$exam_sql  = "
+				SELECT * 
+				FROM `order`
+				WHERE UserID = '$id'
+				";
+	$chk_rlt=mysqli_query($dblink,$exam_sql); if (FALSE===$chk_rlt) echo __LINE__ . mysqli_error($dblink);
+	$idx=0;
+	echo $id;
+	while($row = mysqli_fetch_assoc($chk_rlt)) { #OrderNum ProductID UserID Amount ReceiveAddress ReceiveName ReceivePhone Payment Sent Bill
+		$rtn['ID'][$idx]=$row['ID'];
+		echo $row['ID'];
+		$rtn['OrderNum'][$idx]=$row['OrderNum'];
+		$rtn['ProductID'][$idx]=$row['ProductID'];
+		$rtn['UserID'][$idx]=$row['UserID'];
+		$rtn['Amount'][$idx]=$row['Amount'];
+		$rtn['ReceiveAddress'][$idx]=$row['ReceiveAddress'];
+		$rtn['ReceiveName'][$idx]=$row['ReceiveName'];
+		$rtn['ReceivePhone'][$idx]=$row['ReceivePhone'];
+		$rtn['Payment'][$idx]=$row['Payment'];
+		$rtn['Sent'][$idx]=$row['Sent'];
+		$rtn['Bill'][$idx]=$row['Bill'];
+		$idx++;
+	}
+	return $rtn;
 }
 
 // 更新會員資料
@@ -296,4 +355,6 @@ function getPriceAndName($id){
 	}
 	return $rtn;
 }
+
+
 ?>
