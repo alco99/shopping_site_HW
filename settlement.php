c6cab9623eb57ab70ae56aee615b179a8c87b6c2
alco99 <76519958+alco99@users.noreply.github.com>
2022-05-29 17:28:44 +0800

    Update settlement.php

--- settlement.php
+++ settlement.php
@@ -4,22 +4,50 @@
 	<head>
 		<meta charset="UTF-8">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
 		<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+		<script type="text/javascript" src="js/jquery.form.min.js"></script> 
 		<title>購物車</title>
 		<?php # 一些 PHP 程式碼
-		require_once 'db_connect.php';
-		require_once 'function.php';
-		
-		
-		if (isset($_POST["checkout"]) && ($_POST["checkout"]!="")) {
-			header("Location:check.php"); 
-		}
+			require_once 'db_connect.php';
+			require_once 'function.php';
+			
+			
+			if(isset($_POST["Buy"]) && $_POST["Buy"]!=""){
+				if($_POST["count"]==NULL){
+					$count = 0;
+				} else {
+					$count = $_POST["count"] +1;
+				}
+				//echo 'count:'.$count.'<br/>';
+				
+				$calculate = getThisCart($_SESSION['UserID'], $_POST["Buy"]);
+				$seq = explode(",", $_POST["seq"]);
+				$list = explode(",", $_POST["cartList"]);
+				$sum = explode(",", $_POST["sum"]);
+				
+				
+				$sum[$count] = $calculate['ProductPrice'] * $calculate['ProductQuantity'];
+				$seq[$count] = $_POST["ind"];
+				$list[$count] = $_POST["Buy"];
+				
+				$sequence = implode(",", $seq);
+				$cartList = implode(",", $list);
+				$summary = implode(",", $sum);
+				
+			}
+			
+			
+			
 		?>
 	</head>
 	<body>
 		<div class="container">	
 			<?php # 一些 PHP 程式碼
-			//logo
+			
+				
+				
+				//logo
 				echo '<div class="row">';
 					echo '<div class="col-3">';
 						echo '<a href = "index.php" >';
@@ -48,16 +76,50 @@
 				echo '</div>';
 				
 				echo '<div class="row">';//購物車產品列
-					$mem = getMember($_SESSION['Username']);
-					$cart = getAllCart($mem['ID']);
+					
+					echo '<div class="col-1">';
+						echo '勾選';
+					echo '</div>';
+						
+					echo '<div class="col-6">';
+						echo '商品名稱';
+					echo '</div>';
+						
+					echo '<div class="col-3">';
+						echo '單價';
+					echo '</div>';
+						
+					echo '<div class="col-2">';
+						echo '數量';
+					echo '</div>';
+						
+					echo '<br/><br/>';
+					
+				echo '</div>';
+				
+				echo '<div class="row">';
+					echo '<hr/>';
+				echo '</div>';
+				
+				echo '<div class="row">';//購物車產品列
+					$cart = getAllCart($_SESSION['UserID']);
 					
 					for($i = 0; $i < count($cart["ID"]); $i++){
-						
-					
-						echo '<div class="col-1">';//勾勾框
-							echo '<form action = "" method = "post">';
-								echo '<input type = "checkbox" name = "check" value = "" onchange="this.form.submit()">';
-							echo '</form>';
+						echo '<div class="col-1">';
+							$seq = explode(",", $sequence);
+							//print_r($seq);
+							if($seq == NULL || !(in_array($i, $seq))){//勾勾框
+								$sequence = implode(",", $seq);
+								echo '<form action = "" method = "post">';
+									echo '<input type = "hidden" name = "count" value = "'.$count.'">';
+									echo '<input type = "hidden" name = "ind" value = "'.$i.'">';
+									echo '<input type = "hidden" name = "seq" value = "'.$sequence.'">';
+									echo '<input type = "hidden" name = "cartList" value = "'.$cartList.'">';
+									echo '<input type = "hidden" name = "sum" value = "'.$summary.'">';
+									echo '<input type = "checkbox" name = "Buy" value = "'.$cart["ID"][$i].'" onchange="this.form.submit()">';
+									
+								echo '</form>';
+							}
 						echo '</div>';
 						
 						echo '<div class="col-6">';
@@ -77,18 +139,27 @@
 				echo '</div>';
 				
 				echo '<div class="row">';
+					echo '<hr/>';
+				echo '</div>';
+				
+				echo '<div class="row">';
 					//結帳
 					echo '<div class="col-7">';
 						//空白
 					echo '</div>';
 					echo '<div class="col-3">';
 						//總金額計算
-						echo 'NTD  ';
+						$sum = explode(",", $summary);
+						echo 'NTD  '.array_sum($sum);
+						
 					echo '</div>';
 					echo '<div class="col-2">';
 						//結帳頁面跳轉
 						
-						echo '<form action = "" method = "post">';
+						echo '<form action = "checkout.php" method = "post">';
+							echo '<input type = "hidden" name = "seq" value = "'.$sequence.'">';
+							echo '<input type = "hidden" name = "cartList" value = "'.$cartList.'">';
+							echo '<input type = "hidden" name = "sum" value = "'.$summary.'">';
 							echo '<input type = "submit" name = "checkout" value = "checkout">';
 						echo '</form>';
 						
